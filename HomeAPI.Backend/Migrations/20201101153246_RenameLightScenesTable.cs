2aff85adab5b17c969b1460a5397c173aa1b3e53
chwun <ch.wunderlich@yahoo.de>
2020-11-01 16:47:36 +0100

    add db migration (rename light scenes table)

---
+++ HomeAPI.Backend/Migrations/20201101153246_RenameLightScenesTable.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeAPI.Backend.Migrations
+{
+	[ExcludeFromCodeCoverage]
+	public partial class RenameLightScenesTable : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameTable(
+				name: "Lighting_Scenes",
+				newName: "LightScenes"
+			);
+		}
+
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameTable(
+				name: "LightScenes",
+				newName: "Lighting_Scenes"
+			);
+		}
+	}
+}
