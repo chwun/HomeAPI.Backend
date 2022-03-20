fa4de9905306f97dd6d3bc846c0bc30096aeb93d
chwun <ch.wunderlich@yahoo.de>
2022-03-20 18:01:40 +0100

    add XML comments for accounting controllers

--- HomeAPI.Backend/Controllers/Accounting/AccountingCategoriesController.cs
+++ HomeAPI.Backend/Controllers/Accounting/AccountingCategoriesController.cs
@@ -10,6 +10,7 @@ namespace HomeAPI.Backend.Controllers.Accounting
 {
     [Route("api/accounting")]
     [ApiController]
+    [Produces("application/json")]
     public class AccountingCategoriesController : ControllerBase
     {
         private readonly IAccountingCategoriesRepository repository;
@@ -21,7 +22,16 @@ namespace HomeAPI.Backend.Controllers.Accounting
             this.mapper = mapper;
         }
 
+        /// <summary>
+        /// Get all accounting categories
+        /// </summary>
+        /// <param name="asTree">if true, categories are returned as hierarchy; if false, categories are returned as flat list</param>
+        /// <returns>all accounting categories</returns>
+        /// <response code="200">list/tree of categories</response>
+        /// <response code="500">internal error reading categories</response>
         [HttpGet("categories")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<List<AccountingCategoryDto>>> GetCategories([FromQuery] bool asTree)
         {
             var categories = asTree
@@ -43,7 +53,16 @@ namespace HomeAPI.Backend.Controllers.Accounting
             }
         }
 
+        /// <summary>
+        /// Get accounting category by id
+        /// </summary>
+        /// <param name="id">category id</param>
+        /// <returns>category with given id</returns>
+        /// <response code="200">category object</response>
+        /// <response code="404">category doesn't exist</response>
         [HttpGet("categories/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<AccountingCategoryDto>> GetCategory(int id)
         {
             var category = await repository.GetCategory(id);
@@ -56,10 +75,22 @@ namespace HomeAPI.Backend.Controllers.Accounting
             return Ok(mapper.Map<AccountingCategory, AccountingCategoryDto>(category));
         }
 
+        /// <summary>
+        /// Add a new accounting category
+        /// </summary>
+        /// <param name="categoryDto">New category object</param>
+        /// <param name="parentId">id of parent category (optional)</param>
+        /// <returns>newly created category object</returns>
+        /// <response code="201">newly created category object</response>
+        /// <response code="400">invalid category object or non-existing parent id</response>
+        /// <response code="500">internal error creating category</response>
         [HttpPost("categories")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<AccountingCategoryDto>> AddCategory(
             [FromBody] AccountingCategoryUpdateDto categoryDto,
-            [FromQuery] int parentId)
+            [FromQuery] int? parentId)
         {
             var category = mapper.Map<AccountingCategoryUpdateDto, AccountingCategory>(categoryDto);
 
@@ -68,9 +99,9 @@ namespace HomeAPI.Backend.Controllers.Accounting
                 return BadRequest("Invalid category data");
             }
 
-            if (parentId > 0)
+            if (parentId.Value > 0)
             {
-                var parentCategory = await repository.GetCategory(parentId);
+                var parentCategory = await repository.GetCategory(parentId.Value);
 
                 if (parentCategory is null)
                 {
@@ -92,7 +123,19 @@ namespace HomeAPI.Backend.Controllers.Accounting
             return CreatedAtAction(nameof(GetCategory), new { id = resultCategoryDto.Id }, resultCategoryDto);
         }
 
+        /// <summary>
+        /// Update accounting category by id
+        /// </summary>
+        /// <param name="id">category id</param>
+        /// <param name="categoryDto">category object</param>
+        /// <returns></returns>
+        /// <response code="200">successfully updated category</response>
+        /// <response code="400">invalid category object</response>
+        /// <response code="404">category with given id doesn't exist</response>
         [HttpPut("categories/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> UpdateCategory(int id, [FromBody] AccountingCategoryUpdateDto categoryDto)
         {
             if (categoryDto is null)
@@ -112,7 +155,18 @@ namespace HomeAPI.Backend.Controllers.Accounting
             return Ok();
         }
 
+        /// <summary>
+        /// Delete category by id
+        /// </summary>
+        /// <param name="id">category id</param>
+        /// <returns></returns>
+        /// <response code="200">successfully deleted category</response>
+        /// <response code="400">sub-categories have to be deleted first</response>
+        /// <response code="404">category with given id doesn't exist</response>
         [HttpDelete("categories/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteCategory(int id)
         {
             var category = await repository.GetCategoryWithSubCategories(id);
@@ -124,7 +178,7 @@ namespace HomeAPI.Backend.Controllers.Accounting
 
             if (category.SubCategories?.Count > 0)
             {
-                // only allow deleting category if it has no subcategories
+                // only allow deleting category if it has no sub-categories
                 return BadRequest("Deletion of category with sub-categories not allowed");
             }
 
--- HomeAPI.Backend/Controllers/Accounting/AccountingEntriesController.cs
+++ HomeAPI.Backend/Controllers/Accounting/AccountingEntriesController.cs
@@ -10,6 +10,7 @@ namespace HomeAPI.Backend.Controllers.Accounting
 {
     [Route("api/accounting")]
     [ApiController]
+    [Produces("application/json")]
     public class AccountingEntriesController : ControllerBase
     {
         private readonly IAccountingCategoriesRepository categoriesRepository;
@@ -26,9 +27,27 @@ namespace HomeAPI.Backend.Controllers.Accounting
             this.mapper = mapper;
         }
 
+        /// <summary>
+        /// Get all accounting entries of given category
+        /// </summary>
+        /// <param name="categoryId">category id</param>
+        /// <returns>all entries of given category</returns>
+        /// <response code="200">list of entry objects</response>
+        /// <response code="404">category with given id doesn't exist</response>
+        /// <response code="500">internal error reading entries</response>
         [HttpGet("categories/{categoryId:int}/entries")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<List<AccountingEntryDto>>> GetEntries(int categoryId)
         {
+            var category = await categoriesRepository.GetCategory(categoryId);
+
+            if (category is null)
+            {
+                return NotFound($"Category {categoryId} not found");
+            }
+
             var entries = await entriesRepository.GetEntries(categoryId);
 
             if (entries is null)
@@ -39,7 +58,16 @@ namespace HomeAPI.Backend.Controllers.Accounting
             return Ok(mapper.Map<List<AccountingEntry>, List<AccountingEntryDto>>(entries));
         }
 
+        /// <summary>
+        /// Get accounting entry by id
+        /// </summary>
+        /// <param name="id">entry id</param>
+        /// <returns>entry with given id</returns>
+        /// <response code="200">entry object</response>
+        /// <response code="404">entry with given id doesn't exist</response>
         [HttpGet("entries/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<AccountingEntryDto>> GetEntry(int id)
         {
             AccountingEntry entry = await entriesRepository.GetEntry(id);
@@ -52,7 +80,19 @@ namespace HomeAPI.Backend.Controllers.Accounting
             return Ok(mapper.Map<AccountingEntry, AccountingEntryDto>(entry));
         }
 
+        /// <summary>
+        /// Add a new accounting entry
+        /// </summary>
+        /// <param name="categoryId">id of category, to which entry is added</param>
+        /// <param name="entryDto">entry object</param>
+        /// <returns>newly created entry object</returns>
+        /// <response code="201">newly created new entry</response>
+        /// <response code="400">invalid entry object or non-existing category id</response>
+        /// <response code="500">internal error creating entry</response>
         [HttpPost("categories/{categoryId:int}/entries")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<AccountingEntryDto>> AddEntry(int categoryId, [FromBody] AccountingEntryUpdateDto entryDto)
         {
             var entry = mapper.Map<AccountingEntryUpdateDto, AccountingEntry>(entryDto);
@@ -83,7 +123,19 @@ namespace HomeAPI.Backend.Controllers.Accounting
             return CreatedAtAction(nameof(GetEntry), new { id = resultEntryDto.Id }, resultEntryDto);
         }
 
+        /// <summary>
+        /// Update accounting entry by id
+        /// </summary>
+        /// <param name="id">entry id</param>
+        /// <param name="entryDto">entry object</param>
+        /// <returns></returns>
+        /// <response code="200">successfully updated entry</response>
+        /// <response code="400">invalid entry object</response>
+        /// <response code="404">entry with given id doesn't exist</response>
         [HttpPut("entries/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> UpdateEntry(int id, [FromBody] AccountingEntryUpdateDto entryDto)
         {
             if (entryDto is null)
@@ -104,7 +156,16 @@ namespace HomeAPI.Backend.Controllers.Accounting
         }
 
 
+        /// <summary>
+        /// Delete accounting entry by id
+        /// </summary>
+        /// <param name="id">entry id</param>
+        /// <returns></returns>
+        /// <response code="200">successfully deleted entry</response>
+        /// <response code="404">entry with given id doesn't exist</response>
         [HttpDelete("entries/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteEntry(int id)
         {
             var entry = await entriesRepository.GetEntry(id);
